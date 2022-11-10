75573b226d219920eaa4aaa3b5850496cbb2234f
xie <1298568873@qq.com>
2022-11-10 14:05:10 +0800

    链表

--- src/main/java/LinkedListStruture.java
+++ src/main/java/LinkedListStruture.java
@@ -1,6 +1,7 @@
+import java.util.LinkedList;
 
 //从尾部到头打印链表
-public class LinkedList {
+public class LinkedListStruture {
 
     /**
      * Definition for singly-linked list.
@@ -19,7 +20,11 @@ public class LinkedList {
         listNode2.setNext(listNode3);
         listNode3.setNext(null);
 
+        //方法1：递归法
         reversePrint(listNode);
+
+        //方法2：辅助栈法
+        assistStack(listNode);
     }
 
     //1,2,3 -> 3,2,1
@@ -37,11 +42,28 @@ public class LinkedList {
         ListNode next = listNode.next;
         if (next != null) {
             recursion(next, head);
+            System.out.println(next);
         }
-        System.out.println(next);
-
         if (head == listNode) {
             System.out.println(head);
+        }
+    }
+
+    //方法一：辅助栈，利用栈的先入后出特点
+    public static void assistStack(ListNode listNode) {
+        LinkedList<Integer> linkedList = new LinkedList();
+
+        linkedList.add(listNode.val);
+
+        ListNode next = listNode.next;
+
+        while (next != null) {
+            linkedList.add(next.val);
+            next = next.next;
+        }
+
+        while (linkedList.peek() != null) {
+            System.out.println(linkedList.removeLast());
         }
     }
 
@@ -64,4 +86,5 @@ public class LinkedList {
                     '}';
         }
     }
+
 }
